743780a1f07974300eb22bd4f25cfb6892e8e289
hchiam <hchiam@users.noreply.github.com>
2025-11-02 19:20:05 -0700

    run python3 instead of python

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.listen(port, function () {
 app.get("/", function (req, res) {
   const scriptPath = "./script.py";
   const spawn = require("child_process").spawn;
-  const process = spawn("python", [scriptPath]);
+  // const process = spawn("python", [scriptPath]);
+  const process = spawn("python3", [scriptPath]);
   process.stdout.on("data", function (data) {
     // res.send(data.toString());
     res.send({ result: data.toString() });
